1f061c832e1299df88bd53ee8c5ecd95818472a2
unknown <alexandrannance@gmail.com>
2021-01-17 09:15:48 -0600

    client side

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -37,6 +37,34 @@ const resolvers = {
 
     },
     Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+          
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+          
+            if (!user) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+          
+            const correctPw = await user.isCorrectPassword(password);
+          
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+          
+            const token = signToken(user);
+            return { token, user };
+        },
+        saveBook : async (parent, args) => {
+
+        },
+        deleteBook : async () => {
+            
+        }
 
     }
 };
