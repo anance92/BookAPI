b7537bba277f0d9f70805226d15188003aaec2a2
unknown <alexandrannance@gmail.com>
2021-01-17 08:05:17 -0600

    add schemas

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,44 @@
+const { User, Book } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+              const userData = await User.findOne({ _id: context.user._id })
+                .select('-__v -password')
+                .populate('books');
+          
+              return userData;
+            }
+          
+            throw new AuthenticationError('Not logged in');
+          },
+          // get all users
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+                .populate('books');
+        },
+        // get a user by username
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+                .select('-__v -password')
+                .populate('books');
+        },
+        books: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Book.find(params).sort({ createdAt: -1 });
+        },
+        book: async (parent, { _id }) => {
+            return Book.findOne({ _id });
+        }
+
+    },
+    Mutation: {
+
+    }
+};
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,50 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type Query {
+        me: User
+        // users: [User]
+        // user(username: String!): User
+    }
+
+    input BookInput {
+        bookAuthor: [String]
+        description: String
+        title: String
+        bookId: ID!
+        image: String
+        link: String
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: BookInput): User
+        removeBook(bookId: ID!): User
+    }
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+      }
+
+    type Book {
+        bookId: ID!
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+`;
+
+module.exports = typeDefs;
